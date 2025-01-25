1ee2bf7d38e5c7974562a48d7e76fdd4396a073a
kaankanadasi <kaan.kanadasi@hisarschool.k12.tr>
2025-01-24 22:25:21 -0500

    algorithms

---
+++ Algorithms/Chapter1/Chapter1.1/Histogram.java
@@ -0,0 +1,10 @@
+public class Histogram {
+    public static void main(String[] args) {
+        int[] entry = {1, 2, 3, 4, 5};
+        histogram(entry, 3);
+    }
+
+    public static int[] histogram(int[] a, int m) {
+
+    }
+}
---
+++ Algorithms/Chapter1/Chapter1.1/commandLine.java
@@ -0,0 +1,17 @@
+public class commandLine {
+    public static void main(String[] args) {
+        if (args.length == 3) {
+            int first = Integer.parseInt(args[0]);
+            int second = Integer.parseInt(args[1]);
+            int third = Integer.parseInt(args[2]);
+
+            if (first == second && second == third) {
+                System.out.println("equal");
+            } else {
+                System.out.println("not equal");
+            }
+        } else {
+            System.out.println("Please input exactly 3 command-line arguments");
+        }
+    }
+}
